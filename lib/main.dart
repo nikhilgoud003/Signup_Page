037ddb8bdd26006178c5b758fdf3ab8df66f8180
nikhilgoud003 <nikhilgoud2001@gmail.com>
2025-03-24 18:13:20 -0400

    Initial Project Setup and Import Statements

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+
+void main() {
+  runApp(const SignupValidationApp());
+}
+
+class SignupValidationApp extends StatelessWidget {
+  const SignupValidationApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Signup Validation',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const SignupForm(),
+    );
+  }
+}
